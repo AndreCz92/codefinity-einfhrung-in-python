fb8cd44c0c1a37e10a71762ded75cc58f3d26eac
AndreCz92 <>
2026-02-07 17:54:25 +0000

    Herausforderung: Preisänderung Abschlussprojekt

---
+++ andere_datentypen/herausforderung_preis_nderung_abschlussprojekt/main.py
@@ -0,0 +1,19 @@
+groce_inventory = {"Milk":("Dairy", 3.50, 8),
+                    "Eggs":("Dairy", 5.50, 30),
+                    "Bread":("Bakery", 2.99, 15),
+                    "Apples":("Produce", 1.50, 50)}
+
+#T2
+if 
+
+#T3
+groce_inventory.update({"Tomatoes":("Produce", 1.20, 30)})
+print("Inventory after adding Tomatoes:", groce_inventory)
+
+#T4
+
+
+#T5
+
+#T6
+print("Updated inventory:", groce_inventory)
